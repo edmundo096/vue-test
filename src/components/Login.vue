e4e877dd332d3f011cd4dcde16fce6a156343167
Edmundo Elizondo <edmundo.geg@hotmail.com>
2018-03-22 16:07:14 -0700

    Add login component

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>Login</h1>
+    <h3>{{ msg }}</h3>
+    <div class="login-box">
+      <input type="text" v-model="username">
+      <input type="password" v-model="password">
+      <button type="button" v-on:click="login">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Please login',
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () {
+      this.username;
+      this.password;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+.login-box {
+  margin-left: auto;
+  margin-right: auto;
+  width: 25%;
+  min-width: 300px;
+}
+input, button {
+  display: block;
+}
+input {
+  width: 100%
+}
+</style>
