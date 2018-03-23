a727a96ac81d2d7bca5586f6b761f715fe08a1be
Edmundo Elizondo <edmundo.geg@hotmail.com>
2018-03-22 17:07:40 -0700

    Add weather API logic

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>Welcome</h1>
+    <h3>{{msg}}</h3>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+
+const API_KEY = '1e53985cce01955957837a83c43ac02d'
+const API_URL = 'http://api.openweathermap.org/data/2.5/weather'
+let city = 'London,uk'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome'
+      msg: 'Loading API'
     }
   },
   created () {
@@ -19,8 +24,22 @@ export default {
   },
   methods: {
     fetchData () {
-      axios.get('/').then((response) => {
+      axios({
+        method: 'get',
+        url: API_URL,
+        params: {
+          appid: API_KEY,
+          q: city
+        }
+      }).then((response) => {
         console.log(response)
+        let weather = response.data.weather[0].main
+        let description = response.data.weather[0].description
+        let city = response.data.name
+
+        this.msg = `${city} weather: ${weather} (${description})`
+      }).catch(reason => {
+        this.msg = 'Error getting API data'
       })
     }
   }
